d95c6c5e27ea75241879ef4713acf5d53a552d21
Emmanuelmg19 <78052526+Emmanuelmg19@users.noreply.github.com>
2024-09-18 20:52:46 -0600

    CREACIÓN 5 USUARIOS

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Group;
 use App\Models\Level;
+use App\Models\Profile;
+use App\Models\Location;
+use App\Models\Image;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,12 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-    
         Group::factory()->count(3)->create();
 
         Level::factory()->create(['name' => 'Oro']);
         Level::factory()->create(['name' => 'Plata']);
         Level::factory()->create(['name' => 'Bronce']);
+
+        User::factory()->count(5)->create()->each(function ($user) {
+            
+            $profile = $user->profile()->save(Profile::factory()->make());
+            $profile->location()->save(Location::factory()->make());
+
+            
+            $user->groups()->attach($this->array(rand(1, 3)));
+
+            $user->image()->save(Image::factory()->make([
+                'url' => 'https://lorempixel.com/90/90/'
+            ]));
+        });
+    }
+
+    public function array($max)
+    {
+        $values = [];
+
+        for ($i = 1; $i <= $max; $i++) {
+            $values[] = $i;
+        }
+
+        return $values;
     }
 }
-
